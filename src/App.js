02f19c36b2e7aed30133ef9558f8b9f247bcde75
Noorina <noorinaessa@gmail.com>
2024-03-18 12:04:56 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './component/Nav';
+import Home from './component/Home';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      {/* Other content of your application */}
+    </div>
+  );
+}
+
+export default App;
